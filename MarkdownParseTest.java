1261ba168e5102ae1fadf6236a4b17ec336de1a8
codyprupp <crupp@ucsd.edu>
2022-02-25 13:41:39 -0800

    added snippet tests for lab report 4

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -18,5 +18,28 @@ public class MarkdownParseTest {
         List<String> expect = List.of("https://something.com", "some-page.html");
         assertEquals(MarkdownParse.getLinks(contents), expect);
     }
-    
+
+    @Test
+    public void snippet1Test()
+    {
+        List<String> expected = List.of("%60google.com", "google.com", "ucsd.edu");
+        List<String> actual = MarkdownParse.getLinks("snippet1.md");
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void snippet2Test()
+    {
+        List<String> expected = List.of("a.com", "a.com(())", "example.com");
+        List<String> actual = MarkdownParse.getLinks("snippet2.md");
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void snippet3Test()
+    {
+        List<String> expected = List.of("https://www.twitter.com", "https://ucsd-cse15l-w22.github.io/", "https://cse.ucsd.edu/");
+        List<String> actual = MarkdownParse.getLinks("snippet3.md");
+        assertEquals(expected, actual);
+    }
 }
